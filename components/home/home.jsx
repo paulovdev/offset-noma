fb84a23de612d41e6a16e4b323db9e5ac6966da5
paulovdev <fudevil@gmail.com>
2026-09-08 21:07:28 -0300

    new

--- components/home/home.jsx
+++ components/home/home.jsx
@@ -80,12 +80,30 @@ export default function Home() {
     setCurrentProject(null);
   };
 
+  useEffect(() => {
+    document.documentElement.style.overflow = "hidden";
+    document.body.style.overflow = "hidden";
+    document.body.style.position = "fixed";
+    document.body.style.inset = "0";
+    document.body.style.width = "100%";
+    document.body.style.height = "100%";
+
+    return () => {
+      document.documentElement.style.overflow = "";
+      document.body.style.overflow = "";
+      document.body.style.position = "";
+      document.body.style.inset = "";
+      document.body.style.width = "";
+      document.body.style.height = "";
+    };
+  }, []);
+
   return (
     <>
       <Loader loading={loading} />
       <main
         ref={containerRef}
-        className="relative h-dvh w-full overflow-hidden bg-s cursor-s-resize select-none max-lg:cursor-ew-resize"
+        className="relative h-screen w-full overflow-hidden overscroll-none bg-s cursor-s-resize select-none touch-none max-lg:cursor-ew-resize"
       >
         {/* LEFT PROJECTS */}
         <div
